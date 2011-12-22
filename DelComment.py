71d3b4d3b487533ec01ec1e6485c95737098b461
Duncan Atkin <duncan@datkin.co.uk>
2011-12-22 17:06:26 +0000

    $1

---
+++ DelComment.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+import fbconsole
+fbconsole.APP_ID = '177876045644347'
+fbconsole.AUTH_SCOPE = ['offline_access', 'publish_stream', 'publish_checkins', 'read_stream']
+fbconsole.authenticate()
+id = sys.argv[1]
+print('Deleting ID '+id)
+fbconsole.delete('/'+id)
+
+
+
---
+++ PostStatusMsg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import sys
+import fbconsole
+fbconsole.APP_ID = '177876045644347'
+fbconsole.AUTH_SCOPE = ['publish_stream', 'publish_checkins', 'read_stream', 'offline_access']
+fbconsole.authenticate()
+message = sys.argv[1]
+status = fbconsole.post('/me/feed', {'message': message})
