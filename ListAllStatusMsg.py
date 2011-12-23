dc71bcceea118e0a55a25154a037a57a720fbc86
Duncan Atkin <duncan@datkin.co.uk>
2011-12-23 12:55:54 +0000

    removing extra code which was commented out

--- ListAllStatusMsg.py
+++ ListAllStatusMsg.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python
-
 import fbconsole
 fbconsole.APP_ID = '177876045644347'
 fbconsole.AUTH_SCOPE = ['publish_stream', 'publish_checkins', 'read_stream', 'offline_access']
 fbconsole.authenticate()
 me = fbconsole.get('/me')
 my_id = me['id']
-
-
-#status = fbconsole.post('/me/feed', {'message':'T2'})
-#print "THIS IS THE FUCKING ID RIGHT HERE %s" % status['id']
-
-
-
-
-#status = fbconsole.post('/me/feed', {'message':'Test from remote'})
-
 statuses = fbconsole.fql("SELECT status_id,message FROM status WHERE uid=me()")
 for status in statuses:
     s = fbconsole.get('/%s' % status['status_id'])
