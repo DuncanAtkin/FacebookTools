c1d0c307020180e7ca0ab96122e402a889a13860
Duncan Atkin <duncan@datkin.co.uk>
2012-01-14 01:15:41 +0000

    Adding confirmation of deletion

--- DelComment.py
+++ DelComment.py
@@ -2,7 +2,7 @@
 import sys
 import fbconsole
 fbconsole.APP_ID = '177876045644347'
-fbconsole.AUTH_SCOPE = ['offline_access', 'publish_stream', 'read_stream']
+fbconsole.AUTH_SCOPE = ['offline_access', 'publish_stream', 'read_stream', 'user_photos']
 fbconsole.authenticate()
 id = sys.argv[1]
 print('Deleting ID '+id)
